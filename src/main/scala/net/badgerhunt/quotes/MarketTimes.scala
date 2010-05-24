0f149751c00b62574f0550a97dcca11bb0c11192
Jeremy Mawson <jem.mawson@gmail.com>
2010-05-24 10:43:39 +1000

    Market starts at 10am

--- src/main/scala/net/badgerhunt/quotes/MarketTimes.scala
+++ src/main/scala/net/badgerhunt/quotes/MarketTimes.scala
@@ -7,6 +7,6 @@ object MarketTimes {
   val WORKING_DAYS = Set(MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY)
   def marketIsOpen = {
     val now = new GregorianCalendar
-    WORKING_DAYS.contains(now.get(DAY_OF_WEEK)) && now.get(HOUR_OF_DAY) > 10 && now.get(HOUR_OF_DAY) < 16
+    WORKING_DAYS.contains(now.get(DAY_OF_WEEK)) && now.get(HOUR_OF_DAY) >= 10 && now.get(HOUR_OF_DAY) < 16
   }
 }
