431ed6887f1b20a27796d77ed80867ed61decb05
Jeremy Mawson <jem.mawson@gmail.com>
2010-06-08 13:53:08 +1000

    add daily reminder email

--- src/main/scala/net/badgerhunt/quotes/Watcher.scala
+++ src/main/scala/net/badgerhunt/quotes/Watcher.scala
@@ -5,6 +5,7 @@ import notify.Email
 
 object Watcher extends Application {
   val TEN_MINUTES = 1000 * 60 * 10l
+  val ONE_DAY = TEN_MINUTES * 6 * 24
 
   val config = ConfigParser.parse
   val emails = config.foldLeft(Set(): Set[String]) { _ + _.email }
@@ -22,6 +23,19 @@ object Watcher extends Application {
 
   val timer = new Timer(false)
   timer.schedule(UpdateAndEmailTask, new Date, TEN_MINUTES)
+  timer.schedule(ReminderEmailTask, new Date, ONE_DAY)
+
+  object ReminderEmailTask extends TimerTask {
+    def run() = {
+      val alertedRulesByEmail = alertedRules.foldLeft(Map[String, List[NotifyRule]]()) { (acc, next) => acc(next.email) = next :: acc.getOrElse(next.email, Nil) }
+      alertedRulesByEmail.foreach{rulesForEmail =>
+        val (email, rules) = rulesForEmail
+        val quotes = QuoteSource.current(stocks).foldLeft(Map[String,Double]()){(acc,next)=>acc(next.code)=next.closingPrice}
+        val messages = rules.flatMap(rule => rule.notification(quotes(rule.stock)))
+        if (messages.size > 0) Email.remind(email, messages)
+      }
+    }
+  }
 
   object UpdateAndEmailTask extends TimerTask {
     def run() = {
--- src/main/scala/net/badgerhunt/quotes/notify/Email.scala
+++ src/main/scala/net/badgerhunt/quotes/notify/Email.scala
@@ -12,17 +12,23 @@ object Email {
   properties.put("mail.smtp.host", host)
   println("SMTP Host is %s".format(host))
 
+  def remind(address: String, messages: List[String]) = email(address, "Reminder: %d triggered alerts".format(messages.size), 
+    "This is a daily reminder about the stocks which have breached alert levels:", messages)
 
-  def notify(address: String, messages: List[String]) = {
+  def notify(address: String, messages: List[String]) = email(address, "%d alerts".format(messages.size), 
+    "The following stocks have recently breached their alert level:", messages)
+
+  private def email(address: String, subject: String, intro: String, messages: List[String]) = {
     print("Sending email from %s to %s ... ".format(from, address))
     val session = Session.getDefaultInstance(properties, null)
     val msg = new MimeMessage(session)
     msg.setFrom(new InternetAddress(from))
-    msg.setSubject("[ASXNotify] %d alerts".format(messages.size))
-    msg.setText(messages.sort(_<_).mkString("\n"))
+    msg.setSubject("[ASXNotify] %s".format(subject))
+    msg.setText("%s\n\n%s".format(intro, messages.sort(_<_).mkString("\n")))
     msg.setSentDate(new Date)
     msg.setRecipients(TO, address)
     Transport.send(msg)
     println("email sent")
   }
 }
+
